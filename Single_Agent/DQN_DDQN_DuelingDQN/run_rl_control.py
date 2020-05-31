d372cd396f4213bd30258f16749afca518fdb786
Hrushikesh Jadhav <hrishikesh.jadhav28@gmail.com>
2020-05-31 20:49:11 +0530

    Update run_rl_control.py

--- Single_Agent/DQN_DDQN_DuelingDQN/run_rl_control.py
+++ Single_Agent/DQN_DDQN_DuelingDQN/run_rl_control.py
@@ -7,22 +7,22 @@ from datetime import datetime
 from tqdm import tqdm
 import pandas as pd
 
-
 import cityflow
 from cityflow_env import CityFlowEnv
-# from test.cityflow_env import CityFlowEnv
 from utility import parse_roadnet, plot_data_lists
 from dqn_agent import DQNAgent, DDQNAgent
 from duelingDQN import DuelingDQNAgent
-# import ray
 
-# os.environ["CUDA_VISIBLE_DEVICES"]="0" # use GPU
+from keras.callbacks import TensorBoard # New
+
+#  Stats settings
+AGGREGATE_STATS_EVERY = 50  # episodes
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
     date = datetime.now().strftime('%Y%m%d_%H%M%S')
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--scenario', type=str, default='PongNoFrameskip-v4')
+
     parser.add_argument('--config', type=str, default='config/global_config.json', help='config file')
     parser.add_argument('--algo', type=str, default='DQN', choices=['DQN', 'DDQN', 'DuelDQN'], help='choose an algorithm')
     parser.add_argument('--inference', action="store_true", help='inference or training')
@@ -32,14 +32,16 @@ def main():
     parser.add_argument('--save_freq', type=int, default=50, help='model saving frequency')
     parser.add_argument('--batch_size', type=int, default=30, help='batchsize for training')
     parser.add_argument('--phase_step', type=int, default=3, help='time of one phase')
-    
+
+def main():
+
     args = parser.parse_args()
 
     # preparing config
     # # for environment
     config = json.load(open(args.config))
     config["num_step"] = args.num_step
-    
+
 
     # config["replay_data_path"] = "replay"
     cityflow_config = json.load(open(config['cityflow_config_file']))
@@ -55,7 +57,7 @@ def main():
     phase_list = config['lane_phase_info'][config["intersection_id"]]['phase']
     config["action_size"] = len(phase_list)
     config["batch_size"] = args.batch_size
-    
+
     logging.info(phase_list)
 
     model_dir = "model/{}_{}".format(args.algo, date)
@@ -73,9 +75,9 @@ def main():
     # parameters for training and inference
     # batch_size = 32
     EPISODES = args.epoch
-    learning_start = 200
-    update_model_freq = args.batch_size
-    update_target_model_freq = 200
+    learning_start = 200 # Main/Online model starts learning (.fit()) after 200 episodes.
+    update_model_freq = args.batch_size # Main model is fit() every 'update_model_freq' steps.
+    update_target_model_freq = 200 # Target model is updated with weights of main model every 200 steps.
 
     if not args.inference:
         # build cityflow environment
@@ -87,17 +89,21 @@ def main():
         if not os.path.exists("model"):
             os.makedirs("model")
         if not os.path.exists("result"):
-            os.makedirs("result") 
+            os.makedirs("result")
         os.makedirs(model_dir)
         os.makedirs(result_dir)
-        
+
         # training
         total_step = 0
         episode_rewards = []
         episode_scores = []
         with tqdm(total=EPISODES*args.num_step) as pbar:
-            
+
             for i in range(EPISODES):
+
+                # Update tensorboard step every episode
+                agent.tensorboard.step = i+1
+
                 # print("episode: {}".format(i))
                 env.reset()
                 # agent.epsilon=0.9
@@ -107,9 +113,9 @@ def main():
                 episode_reward = 0
                 episode_score = 0
                 while episode_length < args.num_step:
-                    
-                    action = agent.choose_action(state) # index of action
-                    action_phase = phase_list[action] # actual action
+
+                    action = agent.choose_action(state) # agent chooses action randomly or based on max q value.
+                    action_phase = phase_list[action] # phase to trigger.
                     # no yellow light
                     next_state, reward = env.step(action_phase) # one step
                     # last_action_phase = action_phase
@@ -118,10 +124,10 @@ def main():
                     episode_reward += reward
                     episode_score += env.get_score()
 
-                    for _ in range(args.phase_step-1):
+                    for _ in range(args.phase_step-1): # default value of phase_step = 3
                         next_state, reward_ = env.step(action_phase)
                         reward += reward_
-                    
+
                     reward /= args.phase_step
 
                     pbar.update(1)
@@ -131,8 +137,15 @@ def main():
                     state = next_state
 
                     # training
-                    if total_step > learning_start and total_step % update_model_freq == 0:
-                        agent.replay()
+                    # if total_step > learning_start and total_step % update_model_freq == 0:
+                    #     agent.replay() # fit() the main model
+
+                    if total_step > learning_start:
+                        if episode_length == args.num_step:
+                            terminal_state = True
+                        else:
+                            terminal_state = False
+                        agent.replay(terminal_state) # fit() the main model
 
                     # update target Q netwark
                     if total_step > learning_start and total_step % update_target_model_freq == 0:
@@ -148,6 +161,17 @@ def main():
                 # save episode rewards
                 episode_rewards.append(episode_reward)
                 episode_scores.append(episode_score)
+
+                if not (i+1) % AGGREGATE_STATS_EVERY or (i+1) == 1:
+                    average_reward = sum(episode_rewards[-AGGREGATE_STATS_EVERY:])/len(episode_rewards[-AGGREGATE_STATS_EVERY:])
+                    min_reward = min(episode_rewards[-AGGREGATE_STATS_EVERY:])
+                    max_reward = max(episode_rewards[-AGGREGATE_STATS_EVERY:])
+                    agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=agent.epsilon)
+
+                    # Save model, but only when min reward is greater or equal a set value
+                    if min_reward >= MIN_REWARD:
+                        agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
                 print("score: {}, mean reward:{}".format(episode_score, episode_reward/args.num_step))
 
 
@@ -157,7 +181,7 @@ def main():
                         agent.model.save(model_dir + "/{}-{}.h5".format(args.algo, i+1))
                     else:
                         agent.save(model_dir + "/{}-ckpt".format(args.algo), i+1)
-            
+
                     # save reward to file
                     df = pd.DataFrame({"rewards": episode_rewards})
                     df.to_csv(result_dir + '/rewards.csv', index=None)
@@ -169,7 +193,7 @@ def main():
             # save figure
             plot_data_lists([episode_rewards], ['episode reward'], figure_name=result_dir + '/rewards.pdf')
             plot_data_lists([episode_scores], ['episode score'], figure_name=result_dir + '/scores.pdf')
-        
+
 
     else:
         # inference
@@ -179,10 +203,10 @@ def main():
         env.reset()
 
         agent.load(args.ckpt)
-        
+
         state = env.get_state()
         scores = []
-        for i in range(args.num_step): 
+        for i in range(args.num_step):
             action = agent.choose_action(state) # index of action
             action_phase = phase_list[action] # actual action
             next_state, reward = env.step(action_phase) # one step
@@ -201,7 +225,7 @@ def main():
                             .format(i+1, args.num_step, action, reward))
 
         df = pd.DataFrame({"scores": scores})
-        df.to_csv(result_dir + '/scores.csv', index=None) 
+        df.to_csv(result_dir + '/scores.csv', index=None)
         plot_data_lists([scores], ['scores'], figure_name=result_dir + '/scores.pdf')
 
 
