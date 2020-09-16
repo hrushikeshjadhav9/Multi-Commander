b59f61a7328245d2113b52815b8cbdc79c68566a
Hrushikesh Jadhav <hrushikesh.jadhav16@vit.edu>
2020-09-16 20:16:57 +0530

    changes

--- Multi_Agent/QMIX&Rule-based/dqn_agent.py
+++ Multi_Agent/QMIX&Rule-based/dqn_agent.py
@@ -8,7 +8,7 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-import keras.backend.tensorflow_backend as KTF
+# import keras.backend.tensorflow_backend as KTF
 import tensorflow as tf
 import os
 
@@ -16,7 +16,7 @@ import os
 # KTF.set_session(tf.Session(config=tf.ConfigProto(device_count={'gpu':0})))
 
 class DQNAgent(object):
-    def __init__(self, 
+    def __init__(self,
             intersection_id,
             state_size=9,
             action_size=8,
@@ -38,7 +38,7 @@ class DQNAgent(object):
         self.model = self._build_model()
         self.target_model = self._build_model()
         self.update_target_network()
-        
+
         self.phase_list = phase_list
 
     def _build_model(self):
@@ -76,7 +76,7 @@ class DQNAgent(object):
             phase_start_lane_waiting_count = [intersection_info["start_lane_vehicle_count"][lane] for lane  in phase_start_lane] # [num1, num2, ...]
             sum_count = sum(phase_start_lane_waiting_count)
             phase_waiting_count[index] = sum_count
-        
+
         action = np.argmax(phase_waiting_count)
         return action
 
@@ -94,7 +94,7 @@ class DQNAgent(object):
             q_targets.append(target_f)
 
             # self.model.fit(state, target_f, epochs=1, verbose=0)
-        
+
         states = np.reshape(np.array(states), [-1, self.state_size])
         q_targets = np.reshape(np.array(q_targets), [-1, self.action_size])
         self.model.fit(state, target_f, epochs=2, verbose=0) # batch training
@@ -121,17 +121,17 @@ class DDQNAgent(DQNAgent):
         q_targets = []
         for state, action, reward, next_state in minibatch:
             # compute target value, this is the key point of Double DQN
-            
+
             # target
             target_f = self.model.predict(state)
 
             # choose best action for next state using current Q network
             actions_for_next_state = np.argmax(self.model.predict(next_state)[0])
-            
+
             # compute target value
             target = (reward + self.gamma *
                       self.target_model.predict(next_state)[0][actions_for_next_state] )
-            
+
             target_f[0][action] = target
 
             states.append(state)
@@ -141,25 +141,25 @@ class DDQNAgent(DQNAgent):
         states = np.reshape(np.array(states), [-1, self.state_size])
         q_targets = np.reshape(np.array(q_targets), [-1, self.action_size])
         self.model.fit(state, target_f, epochs=1, verbose=0) # batch training
-        
+
         if self.epsilon > self.epsilon_min:
             self.epsilon = self.epsilon_decay
 
 class MDQNAgent(object):
     def __init__(self,
-        intersection, 
+        intersection,
         state_size=17,
         batch_size=32,
         phase_list={},
         env=None):
-        
+
         self.intersection = intersection
         self.agents =  {}
         self.make_agents(intersection, state_size, batch_size, phase_list, env)
 
     def make_agents(self, intersection, state_size, batch_size, phase_list, env):
-        for id_ in self.intersection: 
-            self.agents[id_] = DQNAgent(id_, 
+        for id_ in self.intersection:
+            self.agents[id_] = DQNAgent(id_,
                                 state_size=state_size,
                                 action_size=len(phase_list[id_]),
                                 batch_size=batch_size,
@@ -177,7 +177,7 @@ class MDQNAgent(object):
                                     action[id_],
                                     reward[id_],
                                     next_state[id_])
-    
+
     def choose_action(self, state):
         action = {}
         for id_ in self.intersection:
@@ -196,4 +196,3 @@ class MDQNAgent(object):
     def save(self, name):
         for id_ in self.intersection:
             self.agents[id_].save(name + '.' + id_)
-
